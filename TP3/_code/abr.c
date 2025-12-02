6c5e86b313b8a521618d8bcb3a9ac3ef58417418
Arthur Marsaro <arthurmarsaro@wificampus-036002.grenet.fr>
2025-12-02 12:01:24 +0100

    [Q2] Cherche done

--- TP3/_code/abr.c
+++ TP3/_code/abr.c
@@ -11,4 +11,16 @@ abr_vide()
 int
 est_abr_vide(abr *a)
 { return arbre_est_vide(a); }
+
+int abr_cherche(abr *a, int n){ //0 il n'y a pas, 1 il'y a
+    if(arbre_valeur(a) == n) return 1;
+    else if(arbre_valeur(a) < n){
+        if(est_abr_vide(arbre_gauche(a))) return 0;
+        return abr_cherche(arbre_gauche(a), n);
+    }
+    else if(abre_valeur(a) > n){
+        if(est_abr_vide(arbre_droit(a))) return 0;
+        return abr_cherche(arbre_droit(a), n);
+    }
+}
   
